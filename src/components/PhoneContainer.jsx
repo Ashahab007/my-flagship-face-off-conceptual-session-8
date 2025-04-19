94e3c639adbed824738d49ec020de6d0a7a5e06c
Ashahab007 <ashahab007@gmail.com>
2025-04-19 12:31:43 +0600

    added search functionality

--- src/components/PhoneContainer.jsx
+++ src/components/PhoneContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import PhoneCard from "./PhoneCard";
 
-// 1.1 my requirement is showing not all the phone. but upon clicking the show all button it will show all the phones. the button name will be changed to show less. then upon clicking the show less it will show the less phones.
+// 1.1 my requirement is showing not all the phone. but upon clicking the show all button it will show all the phones. the button name will be changed to show less. then upon clicking the show less it will show the less phones and button name will be changed to show all.
 const PhoneContainer = ({ phones }) => {
   console.log(phones);
   // 1.2 declare a state
@@ -10,13 +10,13 @@ const PhoneContainer = ({ phones }) => {
   //   1.4 as the phone will be displayed by toggle the show all button and upon toggling the phone will be displayed conditionally so take another state
   const [showAll, setShowAll] = useState(false);
 
-  //   1.3 use useEffect for rendering
+  //   1.5 use useEffect for rendering
   useEffect(() => {
-    // 1.5 now render the phones data upon showAll condition. if showAll is true all the phones will be shown
+    // 1.6 now render the phones data upon showAll condition. if showAll is true all the phones will be shown
     if (showAll) {
       setDisplayPhones(phones);
 
-      //   1.6 if the showAll false then show only 6 phones
+      //   1.7 if the showAll false then show only 6 phones
     } else {
       setDisplayPhones(phones.slice(0, 6));
     }
@@ -30,11 +30,11 @@ const PhoneContainer = ({ phones }) => {
           <PhoneCard phone={phone} key={phone.id}></PhoneCard>
         ))}
       </div>
-      {/* 1.7 added onClick and toggle with  showAll by setShowAll*/}
+      {/* 1.8 added onClick and toggle with  showAll by setShowAll*/}
       <button
         onClick={() => {
           setShowAll(!showAll);
-          //   1.8 it will help to scroll
+          //   2.0 (optional) it will help to scroll
           if (showAll) window.scrollTo(0, 200);
         }}
         className="relative inline-block text-lg group hover:cursor-pointer mt-3"
@@ -42,7 +42,7 @@ const PhoneContainer = ({ phones }) => {
         <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
           <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
           <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-          {/* changing the show All to showLess and showLess to showAll */}
+          {/* 1.9 changing the button text show All to showLess and showLess to showAll upon clicking*/}
           <span className="relative">{showAll ? "Show Less" : "Show All"}</span>
         </span>
         <span
